77bde4e14711225988de5a833437b799101e50c0
Julio Lugo <juliocesarfy6@gmail.com>
2026-02-06 09:38:11 -0700

    rutas de r y login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'login', component: Login},
+  { path: 'register', component: Register },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
