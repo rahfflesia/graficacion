15e2de92c8cfdd6e825904f126bf9d6d6e9ca0d7
Julio Lugo <juliocesarfy6@gmail.com>
2026-02-06 11:12:28 -0700

    login  html

--- frontend/src/app/auth/registro/registro.spec.ts
+++ frontend/src/app/auth/registro/registro.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Register } from './register';
+import { Registro} from './registro';
 
-describe('Register', () => {
-  let component: Register;
-  let fixture: ComponentFixture<Register>;
+describe('Registro', () => {
+  let component: Registro;
+  let fixture: ComponentFixture<Registro>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Register]
+      imports: [Registro]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Register);
+    fixture = TestBed.createComponent(Registro);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
