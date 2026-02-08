311943122a290f82371906ce5d76db22fb2bbc4c
xPeenPapi <156064701+xPeenPapi@users.noreply.github.com>
2026-02-07 18:02:17 -0700

    css

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { Login } from './auth/login/login';
 import { Registro } from './auth/registro/registro';
 import { SeccionProyectos } from '../componentes/principales/seccion-proyectos/seccion-proyectos';
 import { MainLayout } from '../componentes/layouts/main-layout/main-layout';
+import { ProcesosPrincipales } from '../componentes/principales/procesos-principales/procesos-principales';
 
 export const routes: Routes = [{ path: 'login', component: Login},
+  
   { path: 'registro', component: Registro },
+
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  {path : 'procesos', component: ProcesosPrincipales},
+
   {
     path: '',
     component: MainLayout,
