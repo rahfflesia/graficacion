bc0850da063709aa6f2c56b139e356d872f89177
Julio Lugo <juliocesarfy6@gmail.com>
2026-02-07 20:24:42 -0700

    home avance

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { Registro } from './auth/registro/registro';
 import { SeccionProyectos } from '../componentes/principales/seccion-proyectos/seccion-proyectos';
 import { MainLayout } from '../componentes/layouts/main-layout/main-layout';
 import { ProcesosPrincipales } from '../componentes/principales/procesos-principales/procesos-principales';
+import {Home} from '../componentes/principales/home/home';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'login', component: Login},
   { path: 'registro', component: Registro },
 
