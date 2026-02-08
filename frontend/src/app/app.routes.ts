b2134dc165388d233094aaccb173e406c672aead
Julio Lugo <juliocesarfy6@gmail.com>
2026-02-07 21:19:12 -0700

    Home botones funcionales

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,13 +4,12 @@ import { Registro } from './auth/registro/registro';
 import { SeccionProyectos } from '../componentes/principales/seccion-proyectos/seccion-proyectos';
 import { MainLayout } from '../componentes/layouts/main-layout/main-layout';
 import { ProcesosPrincipales } from '../componentes/principales/procesos-principales/procesos-principales';
-import {Home} from '../componentes/principales/home/home';
+import { Home } from '../componentes/principales/home/home';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'login', component: Login},
+  { path: 'login', component: Login },
   { path: 'registro', component: Registro },
-
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 
   {path : 'procesos', component: ProcesosPrincipales},
