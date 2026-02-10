b8db21c9406f108c28f77cf97ec7db7f93ab6540
xPeenPapi <156064701+xPeenPapi@users.noreply.github.com>
2026-02-09 21:52:00 -0700

    para comenzar a conectar el front con el back

--- frontend/src/componentes/principales/procesos-principales/procesos-principales.ts
+++ frontend/src/componentes/principales/procesos-principales/procesos-principales.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Procesos, Usuario } from '../../../models/proceso.interface';
+import { ProcesosService } from '../../../services/procesos.service';
 
 @Component({
   selector: 'app-procesos-principales',
@@ -6,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './procesos-principales.html',
   styleUrl: './procesos-principales.css',
 })
-export class ProcesosPrincipales {
+export class ProcesosPrincipales  {
+  procesos: Procesos[] = [];
+  usuario: Usuario[] = [];
+  
+  constructor(private procesosService: ProcesosService) { }
 
+  ngOnInit(): void {
+    this.cargarDatos();
+  }
+  cargarDatos(): void {
+    this.procesosService.getUsuarios().subscribe(data =>{
+      this.usuario = data;
+    
+    });
+      this.procesosService.getProcesos().subscribe(data =>{
+        this.procesos = data;
+      });
+  }
+
+  
 }
